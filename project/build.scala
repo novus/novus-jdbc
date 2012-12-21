24a100074a9c97facad890da082a00a1961dcb47
oreese <oreese@novus.com>
2012-12-21 11:35:36 -0500

    Version bump

--- project/build.scala
+++ project/build.scala
@@ -8,7 +8,7 @@ object NovusjdbcBuild extends sbt.Build {
             base = file("."),
             settings = Project.defaultSettings ++ Seq(
               organization := "com.novus",
-              version := "0.2.0-SNAPSHOT",
+              version := "0.3.0-SNAPSHOT",
               scalaVersion := "2.9.2",
               crossScalaVersions := Seq("2.8.1", "2.9.0", "2.9.0-1", "2.9.1"),
               initialCommands := "import com.novus.jdbc._",
