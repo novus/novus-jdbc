25a86c438dd74d66ffd56848fa9ac406132668fa
oreese <oreese@novus.com>
2013-01-24 09:16:17 -0500

    PULSE-4 another unit test case just in case

--- src/test/scala/com/novus/jdbc/QueryableSpec.scala
+++ src/test/scala/com/novus/jdbc/QueryableSpec.scala
@@ -51,4 +51,26 @@ class QueryableSpec extends Specification with Mockito{
       there was one(stmt).setNull(1, Types.NULL)
     }
   }
+
+  "replace" should{
+    val able = new Queryable[TestDB]{}
+
+    "handle multiple iterable types" in{
+      val matcher = able.questionMark.matcher("SELECT a FROM b WHERE c in(?) AND d in(?)")
+
+      able.replace(List(List(1,2), List(3,4)), matcher) must be equalTo "SELECT a FROM b WHERE c in(?,?) AND d in(?,?)"
+    }
+
+    "handle an iterable with one item" in{
+      val matcher = able.questionMark.matcher("SELECT a FROM b WHERE c in(?)")
+
+      able.replace(List(List(1)), matcher) must be equalTo "SELECT a FROM b WHERE c in(?)"
+    }
+
+    "handle non-iterables" in{
+      val matcher = able.questionMark.matcher("SELECT a FROM b WHERE c in(?)")
+
+      able.replace(List(1), matcher) must be equalTo "SELECT a FROM b WHERE c in(?)"
+    }
+  }
 }
