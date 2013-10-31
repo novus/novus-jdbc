5618a37b8724592cee7bef856aab9d39d293257f
Max Afonov <max@novus.com>
2013-10-30 21:58:50 -0400

    bump

--- project/build.scala
+++ project/build.scala
@@ -81,7 +81,7 @@ object NovusjdbcBuild extends sbt.Build {
 
   lazy val baseSettings = Project.defaultSettings ++ Seq(
     organization := "com.novus",
-    version := "0.9.6-FINAL",
+    version := "0.9.7-SNAPSHOT",
     scalaVersion := "2.10.3",
     initialCommands := "import com.novus.jdbc._",
     scalacOptions := Seq("-deprecation", "-unchecked", "-feature", "-language:postfixOps"),
