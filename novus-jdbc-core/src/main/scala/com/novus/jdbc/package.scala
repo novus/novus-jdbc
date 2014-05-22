30dadd6fac6760188c8be096b4b72b2694802d6c
Max Afonov <max@bumnetworks.com>
2014-05-22 00:50:59 -0400

    removed unused code

--- novus-jdbc-core/src/main/scala/com/novus/jdbc/package.scala
+++ novus-jdbc-core/src/main/scala/com/novus/jdbc/package.scala
@@ -27,19 +27,4 @@ object `package`{
   type Postgres = com.novus.jdbc.postgres.Postgres
   type Sqlite = com.novus.jdbc.sqlite.Sqlite
   type Sybase = com.novus.jdbc.sybase.Sybase
-
-  /**
-   * Helper method to ensure that `close` is called after using a `CloseableIterator` within a function.
-   *
-   * @param iter the iterator
-   * @param f the function applied to the iterator
-   * @tparam A type returned by the iterator
-   * @tparam B type returned by the function invocation
-   */
-  def manage[A,B](iter: CloseableIterator[A])(f: => B): B = try{
-    f
-  }
-  finally{
-    iter close ()
-  }
 }
